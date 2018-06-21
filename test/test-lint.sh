ac18befbf4f2dc7f450b33827a1ef4e5455b4650
Will Washburn <will@tailwindapp.com>
2018-06-21 11:21:13 -0500

    Prevent duplicate class names

--- test/test-lint.sh
+++ test/test-lint.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-${SCRIPT_DIR}/../vendor/bin/phpcs --colors --standard=${SCRIPT_DIR}/../src/Tailwind/Standard/Tailwind ${SCRIPT_DIR}/../test/test-lint.php
+${SCRIPT_DIR}/../vendor/bin/phpcs --colors --standard=${SCRIPT_DIR}/../src/Tailwind/Standard/Tailwind ${SCRIPT_DIR}/../test/linting/
