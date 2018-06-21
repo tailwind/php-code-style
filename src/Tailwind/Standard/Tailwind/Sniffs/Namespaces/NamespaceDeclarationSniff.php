74faa2f7ec9d15f5ee024d81d83bec3895ae8900
Will Washburn <will@tailwindapp.com>
2018-06-21 11:05:28 -0500

    Simplify rules

---
+++ src/Tailwind/Standard/Tailwind/Sniffs/Namespaces/NamespaceDeclarationSniff.php
@@ -0,0 +1,44 @@
+<?php
+
+class Tailwind_Sniffs_Namespaces_NamespaceDeclarationSniff  implements PHP_CodeSniffer_Sniff
+{
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_NAMESPACE);
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        for ( $i = ($stackPtr + 1); $i < ($phpcsFile->numTokens - 1); $i++ ) {
+            if ( $tokens[$i]['line'] === $tokens[$stackPtr]['line'] ) {
+                continue;
+            }
+
+            break;
+        }
+
+        // Make sure this Namespace declaration in same line of open tag.
+        $prev = $phpcsFile->findPrevious(T_OPEN_TAG, ($stackPtr - 1));
+        if ( $tokens[$prev]['line'] !== $tokens[$stackPtr]['line'] ) {
+            $error = 'The namespace declaration and open tag must be in same line';
+            $phpcsFile->addError($error, $stackPtr, 'SameLine');
+        }
+    }
+}
